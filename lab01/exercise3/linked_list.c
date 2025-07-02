b5785bacdea625b908bfe2d32afbd0bae7520112
Ismail <ismailgoraya76@gmail.com>
2025-07-02 15:56:57 +0500

    Solved Exercise 3

--- lab01/exercise3/linked_list.c
+++ lab01/exercise3/linked_list.c
@@ -55,7 +55,7 @@ void print_list(struct Node *head) {
 
 /* Iteratively reverses a linked list whose first node is HEAD */
 void reverse_list(struct Node **head) {
-    if (head == NULL) {
+    if (head == NULL || *head == NULL) {
         return;
     }
     struct Node *curr = *head;
@@ -76,10 +76,19 @@ void add_to_back(Node **head, int data) {
     if (head == NULL) {
         return;
     }
+
     Node *new_node = create_node(data);
-    Node *prev;
-    for (Node *curr = *head; curr != NULL; curr = curr->next) {
-        prev = curr;
+    if (*head == NULL) {
+        // Empty list: new node becomes the head
+        *head = new_node;
+        return;
     }
-    prev->next = new_node;
+
+    Node *curr = *head;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+
+    curr->next = new_node;
 }
+
